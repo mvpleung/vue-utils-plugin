e2337a8035b0c01d31b2feb964ec65cf3269e509
liangzch <mvpleung@gmail.com>
2018-01-26 15:04:15 +0800

    release 0.0.2

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@
  * @Author: liangzc 
  * @Date: 2017-07-31 
  * @Last Modified by: liangzc
- * @Last Modified time: 2018-01-26 10:00:17
+ * @Last Modified time: 2018-01-26 14:58:28
  */
-let configOptions;
+let $Vue, configOptions;
 
 let init = function () {
-    this.$utils = Object.assign({}, require('./ui.tool'), require('./default'), (configOptions || {}).utils || {}); //添加vm实例验证属性
+    $Vue.$utils = this.$utils = Object.assign({}, require('./ui.tool'), require('./default'), (configOptions || {}).utils || {}); //添加vm实例验证属性
 };
 /**
  * @param {Vue} Vue 
  * @param {Object} options {utils: {replace: Function,...}}
  */
 let install = function (Vue, options) {
+    $Vue = Vue;
     configOptions = options;
     Vue.mixin({
         created: init
     });
 };
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(verify);
+    window.Vue.use(install);
 }
 module.exports = install;
