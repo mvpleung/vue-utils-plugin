19b8ecf1acc58ef1748e88df0d099db8e5c698fd
liangzch <mvpleung@gmail.com>
2018-10-16 10:29:58 +0800

    add socket tools

--- src/socket.tool.js
+++ src/socket.tool.js
@@ -3,7 +3,7 @@
  * @Author: liangzc 
  * @Date: 2018-05-26 10:04:03 
  * @Last Modified by: liangzc
- * @Last Modified time: 2018-07-23 12:59:51
+ * @Last Modified time: 2018-09-27 13:49:49
  */
 class _$SocketClient {
   /**
@@ -13,6 +13,8 @@ class _$SocketClient {
    *    heartCheck: Boolean, 是否需要心跳检测，默认为 false,
    *    reconnect: Boolean, 是否需要断线重连，默认为 true,
    *    heartTime: Number, 心跳时间，默认 60秒,
+   *    timeout: Number, 超时时间，默认15分钟,
+   *    reDelay: Number, 重连延迟间隔，默认为30秒,
    *    debug: Boolean 是否为 debug 模式，debug 模式会打印日志
    * }
    */
@@ -21,15 +23,9 @@ class _$SocketClient {
       console.warn('你的浏览器不支持websocket');
       return;
     }
-    this.lockReconnect = false; //避免重复连接
-    this.option = Object.assign(
-      {},
-      { heartCheck: false, reconnect: true, heartTime: 60000 },
-      option || {}
-    );
-    //心跳配置
-    this.timeoutObj = null;
-    this.serverTimeoutObj = null;
+    option.url = url;
+    this.reset(option);
+    this.resetTimeOut();
     this.createSocket(url);
   }
 
@@ -38,38 +34,46 @@ class _$SocketClient {
    * @param {String} url
    */
   createSocket(url) {
+    if (!this.option.url || this.option.url === '' || this.option.url === null)
+      return;
     try {
       this.ws = new WebSocket(url);
 
       this.ws.onopen = event => {
         this.option.debug && console.log('websocket 连接打开', event);
         this.onopen && this.onopen(event, this.ws);
+        this.trigger('open', event);
         //心跳检测重置
-        this.option.heartCheck === true && this.startHeartCheck();
+        this.startHeartCheck();
+        clearTimeout(this.timeoutObj);
       };
 
       this.ws.onmessage = event => {
         this.option.debug && console.log('websocket message', event);
         this.onmessage && this.onmessage(event, this.ws);
+        this.trigger('message', event);
         //如果获取到消息，心跳检测重置
         //拿到任何消息都说明当前连接是正常的
-        this.option.heartCheck === true && this.startHeartCheck();
+        this.startHeartCheck();
+        clearTimeout(this.timeoutObj);
       };
 
       this.ws.onclose = event => {
         this.option.debug && console.log('websocket 连接关闭', event);
         this.onclose && this.onclose(event, this.ws);
-        this.option.reconnect === true && this.reconnect(url);
+        this.trigger('close', event);
+        this.reconnect(url);
       };
 
       this.ws.onerror = event => {
         this.option.debug && console.error('websocket error', event);
         this.onerror && this.onerror(event, this.ws);
-        this.option.reconnect === true && this.reconnect(url);
+        this.trigger('error', event);
+        this.reconnect(url);
       };
     } catch (error) {
       this.option.debug && console.error(error);
-      this.option.reconnect === true && this.reconnect(url);
+      this.reconnect(url);
     }
   }
 
@@ -91,12 +95,72 @@ class _$SocketClient {
   }
 
   /**
+   * 重新打开 Socket
+   */
+  reOpen() {
+    this.reset();
+    this.resetTimeOut();
+    this.createSocket(this.option.url);
+  }
+
+  /**
    * 关闭Socket
    */
   close() {
+    this.reset({ reconnect: false });
+  }
+
+  /**
+   * 重置
+   *
+   * @param {Object} option 配置项
+   */
+  reset(option) {
+    this.lockReconnect = false; //避免重复连接
+    this.option = Object.assign(
+      {},
+      {
+        heartCheck: false,
+        reconnect: true,
+        heartTime: 60000,
+        reDelay: 30000,
+        timeout: 900000
+      },
+      option || {}
+    );
+    //心跳配置
+    clearTimeout(this.heartCheckObj);
+    clearTimeout(this.serverTimeoutObj);
+    clearTimeout(this.timeoutObj);
+    this.heartCheckObj = null;
+    this.serverTimeoutObj = null;
+    this.timeoutObj = null;
+    this.eventPools = {};
     if (this.ws) {
-      this.ws.close();
+      try {
+        this.ws.onerror = null;
+        this.ws.onclose = null;
+        this.ws.close();
+      } catch (error) {}
       this.ws = null;
+    }
+  }
+
+  /**
+   * 重置超时Timer
+   */
+  resetTimeOut() {
+    clearTimeout(this.timeoutObj);
+    if (
+      this.option.reconnect === true &&
+      this.option.url &&
+      this.option.url !== '' &&
+      this.option.url !== null
+    ) {
+      this.timeoutObj = setTimeout(() => {
+        console.warn('socket timeout!');
+        this.close();
+      }, this.option.timeout);
     }
   }
 
@@ -104,24 +168,32 @@ class _$SocketClient {
    * 重建链接
    */
   reconnect(url) {
-    if (this.lockReconnect || !this.ws) return;
+    if (
+      this.option.reconnect !== true ||
+      this.lockReconnect ||
+      !this.ws ||
+      this.timeoutObj === undefined ||
+      this.timeoutObj === null
+    )
+      return;
     this.lockReconnect = true; //加重新链接锁，避免重复请求
     //没连接上会一直重连，设置延迟避免请求过多
     setTimeout(() => {
       this.option.debug && console.log('reconnect....');
       this.createSocket(url);
       this.lockReconnect = false;
-    }, 2000);
+    }, this.option.reDelay);
   }
 
   /**
    * 心跳检测
    */
   startHeartCheck() {
-    clearTimeout(this.timeoutObj);
+    if (this.option.heartCheck !== true) return;
+    clearTimeout(this.heartCheckObj);
     clearTimeout(this.serverTimeoutObj);
     this.ws &&
-      (this.timeoutObj = setTimeout(() => {
+      (this.heartCheckObj = setTimeout(() => {
         //这里发送一个心跳，后端收到后，返回一个心跳消息，
         //onmessage拿到返回的心跳就说明连接正常
         if (this.ws) {
@@ -132,6 +204,67 @@ class _$SocketClient {
           }, this.option.heartTime);
         }
       }, this.option.heartTime));
+  }
+
+  /**
+   * 触发事件
+   */
+  trigger(name, event, socket = this.ws) {
+    let events = this.eventPools[name];
+    if (events && Array.isArray(events) && events.length) {
+      events.forEach(
+        listener =>
+          listener.callback &&
+          listener.callback.call(listener.context, event, socket)
+      );
+    }
+  }
+
+  /**
+   * 事件监听
+   * @param {String} event 事件名称
+   * @param {Function} func 绑定的事件
+   * @param {Object} context 需要绑定的上下文，默认为 null
+   */
+  on(event, func, context = null) {
+    if (event && typeof func === 'function') {
+      let eventName = event.indexOf('on') === 0 ? event.substring(2) : event;
+      let listeners = this.eventPools[eventName] || [];
+      listeners.push({ callback: func, context: context });
+      this.eventPools[eventName] = listeners;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * 取消事件监听
+   * @param {String} event 事件名称
+   * @param {Function} func 取消绑定的事件
+   * @param {Object} context 需要取消绑定的上下文，默认为 null
+   */
+  off(event, func, context = null) {
+    if (event && typeof func === 'function') {
+      let eventName = event.indexOf('on') === 0 ? event.substring(2) : event;
+      let listeners = this.eventPools[eventName],
+        index;
+      if (listeners && Array.isArray(listeners) && listeners.length) {
+        index = listeners.reduce((i, listener, index) => {
+          return typeof listener.callback === 'function' &&
+            listener.callback === func &&
+            listener.context === context ?
+            (i = index) :
+            i;
+        }, -1);
+
+        if (index > -1) {
+          listeners.splice(index, 1);
+          this.eventPools[eventName] = listeners;
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
 
@@ -144,6 +277,8 @@ let SocketTools = (function() {
    *    heartCheck: Boolean, 是否需要心跳检测，默认为 false,
    *    reconnect: Boolean, 是否需要断线重连，默认为 true,
    *    heartTime: Number, 心跳时间，默认 60秒,
+   *    timeout: Number, 超时时间，默认15分钟,
+   *    reDelay: Number, 重连延迟间隔，默认为30秒,
    *    callbacks: Object,  onopen(event), onclose(event), onerror(event), onmessage(event),
    *    debug: Boolean 是否为 debug 模式，debug 模式会打印日志
    * }
